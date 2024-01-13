42e38c074b7b77488e97b5904b1acfd3a1aa23b0
Obaydullah <obaydullahkhaan@gmail.com>
2024-01-13 17:45:45 +0600

    comment bug fix

--- src/main/java/com/obaydullah/blogingappapis/entities/Post.java
+++ src/main/java/com/obaydullah/blogingappapis/entities/Post.java
@@ -39,7 +39,7 @@ public class Post {
     private User user;
 
     @OneToMany(mappedBy = "post",cascade = CascadeType.ALL)
-    private Set<Comment> comment = new HashSet<>();
+    private Set<Comment> comments = new HashSet<>();
 
 
 }
